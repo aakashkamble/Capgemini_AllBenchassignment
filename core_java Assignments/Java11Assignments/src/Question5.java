9a79034507e4f1f4b40784c62818dbb0dc7e009a
Namdev Kamble <akash.namdev-kamble@capgemini.com>
2022-06-24 15:06:32 +0530

    All Core Java Assignments

--- core_java Assignments/Java11Assignments/src/Question5.java
+++ core_java Assignments/Java11Assignments/src/Question5.java
@@ -1,41 +1,53 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 public class Question5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int total=0,option,price,count=1;
-//		var path= "C:\\Users\\THYAGC\\Documents\\price.txt";
-//		var path1="C:\\Users\\THYAGC\\Documents\\total.txt";
-//		String response="yes";
-	//	
-//		Scanner sc=new Scanner(System.in);
-	//	
-//		do {
-//			System.out.println("select option \n 1:insert price \n 2:view purchase 3:exit");
-//			option=sc.nextInt();
-//			
-//			if(option==1) {
-//				while(response.equals("yes")) {
-//					System.out.println("insert price"+count);
-//					count++;
-//					price=sc.nextInt();
-//					total=+price;
-//					
-//					try {
-//						Files.writeString(Path.of(path),csq:price+"\n",StandardOpenOption.APPEND);
-//						
-//					}
-//					catch(IOException e) {}
-//					
-//					sc.nextLine();
-//					System.out.println("do u want to add more items? (yes\no)");
-//					response.equals("no");
-//					break;
-//				}
-//				if(option==2) {
-//					System.out.println(price+" "+total);
-//				}
-
+		Scanner s = new Scanner(System.in);
+        var pathtofile ="C:\\Users\\AKNAMDEV\\Desktop\\Capgemini\\Capgemini_AllBenchassignment\\core_java Assignments\\Java11Assignments\\record.txt";
+        System.out.println("Select Your choice");
+        System.out.println("1}Insert New Price 2}View Purchase Total 3}Exit");
+        int item_number =1;
+        int choice= s.nextInt();
+        boolean b = true;
+        switch (choice){
+            case 1: {
+                System.out.println("Enter the Price of item " + item_number + ": ");
+                String price = s.next();
+                try {
+					Files.writeString(Path.of(pathtofile), price+"\n", StandardOpenOption.APPEND);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                System.out.println("Price has been saved in file");
+                break;
+            }
+            case 2:{
+                Scanner read = null;
+				try {
+					read = new Scanner(Files.readString(Path.of(pathtofile)));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                int total=0;
+                while (read.hasNext()){
+                    total = total+read.nextInt();
+                }
+                System.out.println("Your total is: "+total);
+                break;
+            }
+            case 3:{
+                System.out.println("exit program....");
+                break;
+            }
+        }
 	}
-
 }
+	
