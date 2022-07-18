2a5f36aefec4faf8efc69189f18f9c86421e6549
Kamble <akash.namdev-kamble@capgemini.com>
2022-07-18 23:34:32 +0530

    JWT Authorization

--- Spring Security/spring-security-Assignment1-Q4/src/main/java/com/springSecurity/springsecurityAssignment1Q4/SpringSecurityConfig.java
+++ Spring Security/spring-security-Assignment1-Q4/src/main/java/com/springSecurity/springsecurityAssignment1Q4/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.springSecurity.springsecurityAssignment1Q4;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,8 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.jws.soap.SOAPBinding;
 
 
 @EnableWebSecurity
